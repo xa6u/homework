bc2dcba47de84c28f4f87f5787daa0ce82a9b305
xa6u <siriusitsastar@gmail.com>
2015-10-27 16:19:13 +0200

    testcommit
    testcommit

---
+++ src/main/java/lecture2/ListHomework.java
@@ -0,0 +1,24 @@
+package lecture2;
+
+import java.util.ArrayList;
+
+public class ListHomework {
+
+    int final  = 10;
+    int a;
+    int stringLength;
+
+   ArrayList<String> stringList = new ArrayList<String>() {
+
+       stringLength = stringList.size();
+
+    for (i = 0; i < stringLength; i++ )  {
+
+           stringList.add("FirstElement" + i);
+
+       }
+
+
+   }
+
+}
