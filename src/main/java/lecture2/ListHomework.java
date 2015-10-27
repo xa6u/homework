27e136566deb0cf9f26167b9132cc1529366ce01
xa6u <siriusitsastar@gmail.com>
2015-10-27 19:45:57 +0200

    Final realization of first part of homework
    Final realization of first part of homework

--- src/main/java/lecture2/ListHomework.java
+++ src/main/java/lecture2/ListHomework.java
@@ -2,23 +2,25 @@ package lecture2;
 
 import java.util.ArrayList;
 
-public class ListHomework {
+
+public  class ListHomework {
+
 
     public static void main(String[] args){
 
-    }
-
-    final int CONST = 5;
-    int stringLength;
-    String stringElement;
-    int i;
 
 
-    ArrayList <String> stringList = new ArrayList<String>(); {
+        final int CONST = 5;
+        int stringLength;
+        String stringElement;
+        int i;
+
+
+        ArrayList <String> stringList = new ArrayList<String>();
 
         for (i = 0; i < CONST; i++ ){
 
-            stringList.add("FirstElement" + i);
+            stringList.add("Element " + i);
         }
 
 
@@ -27,8 +29,7 @@ public class ListHomework {
         System.out.println("Размер списка = " + stringLength);
         System.out.println("Третий элемент списка = " + stringElement);
 
+
     }
 
-
 }
-
